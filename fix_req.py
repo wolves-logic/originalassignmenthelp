7d332cc545f972416224948b3d9f3d99fcff8382
saikrish44 <saikrishnark87@gmail.com>
2026-04-17 17:44:03 +0530

    Removing Used Files

--- fix_req.py
+++
@@ -1,9 +0,0 @@
-import sys
-import codecs
-
-with open('requirements.txt', 'r', encoding='utf-16') as f:
-    content = f.read()
-
-with open('requirements.txt', 'w', encoding='utf-8') as f:
-    f.write(content)
-print("done")
